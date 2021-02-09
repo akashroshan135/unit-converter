d23837b43364f5ec5b955512b7bfa8f92242dc49
Akash Roshan <akash.second1999@gmail.com>
2021-02-09 17:26:16 +0530

    edit use stateful widget for converter page

--- lib/routes/converter_route.dart
+++ lib/routes/converter_route.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ConverterScreen extends StatelessWidget {
+class ConverterScreen extends StatefulWidget {
   final categoryName;
   final categoryColor;
 
@@ -10,6 +10,13 @@ class ConverterScreen extends StatelessWidget {
   );
 
   @override
+  _ConverterScreenState createState() => _ConverterScreenState();
+}
+
+class _ConverterScreenState extends State<ConverterScreen> {
+  var _color = Colors.black;
+
+  @override
   Widget build(BuildContext context) {
     final appBar = AppBar(
       leading: IconButton(
@@ -17,10 +24,10 @@ class ConverterScreen extends StatelessWidget {
           // * Navigator pops the old screen from stack
           onPressed: () => Navigator.pop(context)),
       title: Text(
-        categoryName,
+        widget.categoryName,
         style: TextStyle(fontSize: 20, color: Colors.white),
       ),
-      backgroundColor: categoryColor,
+      backgroundColor: widget.categoryColor,
     );
 
     return MaterialApp(
@@ -28,11 +35,14 @@ class ConverterScreen extends StatelessWidget {
       home: Scaffold(
         appBar: appBar,
         body: Container(
-          color: Colors.black,
-          child: Center(
-            child: Text(
-              'Hello World',
-              style: TextStyle(color: Colors.white),
+          color: _color,
+          child: FlatButton(
+            onPressed: () => setState(() => _color = Colors.blue),
+            child: Center(
+              child: Text(
+                'Hello World',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
           ),
         ),
